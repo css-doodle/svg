351bba6e87efa07f2c18565de9c4bc7b11e95459
yuanchuan <yuanchuan23@gmail.com>
2022-10-05 17:58:07 +0800

    format code

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
     serviceWorker: {
       register: true,
     }
-	}
+  }
 };
 
 export default config;
