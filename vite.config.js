780fd1076c9f1672c7b4dc5936ef47cf62e12bf6
yuanchuan <yuanchuan23@gmail.com>
2022-10-09 11:59:22 +0800

    Test yarn

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
   plugins: [
     sveltekit()
-  ],
-
-  build: {
-    rollupOptions: {
-      external: [
-        'css-doodle/generator'
-      ]
-    }
-  }
+  ]
 };
 
 export default config;
