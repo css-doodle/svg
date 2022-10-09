80cd82952edbe70986ca86748f78ebd1596365ab
yuanchuan <yuanchuan23@gmail.com>
2022-10-09 11:48:25 +0800

    add external deps for vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
   plugins: [
     sveltekit()
-  ]
+  ],
+
+  build: {
+    rollupOptions: {
+      external: [
+        'css-doodle'
+      ]
+    }
+  }
 };
 
 export default config;
