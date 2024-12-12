44cd4fda0bf163c9374a9a79a7cada1649f1042c
yuanchuan <yuanchuan23@gmail.com>
2024-12-12 14:46:21 +0800

    Simplify xml code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,16 +89,16 @@
     try {
       let xmlDoc = new DOMParser().parseFromString(input, 'application/xml');
       let xsltDoc = new DOMParser().parseFromString(`
-        <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-          <xsl:strip-space elements="*"/>
-          <xsl:template match="para[content-style][not(text())]">
-            <xsl:value-of select="normalize-space(.)"/>
-          </xsl:template>
-          <xsl:template match="node()|@*">
-            <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>
-          </xsl:template>
-          <xsl:output indent="yes"/>
-        </xsl:stylesheet>
+<xsl:stylesheet version="1.0"
+ xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+ <xsl:output omit-xml-declaration="yes" indent="yes"/>
+
+    <xsl:template match="node()|@*">
+      <xsl:copy>
+        <xsl:apply-templates select="node()|@*"/>
+      </xsl:copy>
+    </xsl:template>
+</xsl:stylesheet>
       `,'application/xml');
 
       let xsltProcessor = new XSLTProcessor();
